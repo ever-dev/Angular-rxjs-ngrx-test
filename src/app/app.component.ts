aaa8a5e80797f27fa2c5f46a4a4e1a20a95da51d
ever-dev <everdev0923@gmail.com>
2019-10-23 13:09:50 -0400

    Task 1: create a long polling with RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Observable, interval, Subscription, fromEvent, merge, of } from 'rxjs';
+import { mapTo } from 'rxjs/operators'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-rxjs-test';
+  poller: Subscription = null;
+
+
+  constructor() {
+    this.getOnlineStatus().subscribe(isConnected => {
+      if (isConnected) {
+        console.log('start polling...');
+        this.poller = interval(1500).subscribe(() => this.pollingFunction())
+      } else {
+        if (this.poller) {
+          console.log('stop polling...');
+          this.poller.unsubscribe();
+        }
+      }
+    })
+  }
+
+  getOnlineStatus(): Observable<boolean> {
+    return merge(
+      of(navigator.onLine),
+      fromEvent(window, 'online').pipe(mapTo(true)),
+      fromEvent(window, 'offline').pipe(mapTo(false))
+    );
+  }
+
+  pollingFunction(): void {
+    console.log('polling...');
+  }
+
 }
